7a8ca7c75e6dbf2ed68bd3d1cfda9f613ddbfa22
tarang-soni <83337255+tarang-soni@users.noreply.github.com>
2024-12-07 15:51:01 +0530

    removed console window

--- MusicGame/MusicGame/src/Main.cpp
+++ MusicGame/MusicGame/src/Main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 #include "Game.h"
-
+#include <windows.h> 
 int main() {
+	HWND window;
+	window = FindWindowA("ConsoleWindowClass", NULL);
+#ifdef NDEBUG
+	ShowWindow(window, 0);
+#elif _DEBUG
+	ShowWindow(window, 1);
+
+#endif // NDEBUG
+
+
 	Game game("Hello World", 800, 800);
 	game.Run();
 	return 0;
